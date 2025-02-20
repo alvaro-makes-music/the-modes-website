c147dd1af793e0251e38a2c5d45a70c34b25ae20
Alvaro <alvarocaceresmunoz@gmail.com>
2025-02-19 21:59:02 -0800

    Removed useEffect from BackgroundImage

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import styles from '@/app/styles/BackgroundImage.module.css';
 
 export default function BackgroundImage() {
