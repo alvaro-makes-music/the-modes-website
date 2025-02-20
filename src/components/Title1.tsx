219d60f4599c08b01bd26d0fa05ce3ba68f8e9b5
Alvaro <alvarocaceresmunoz@gmail.com>
2025-02-20 01:48:09 -0800

    Swaped a to Link on the title1

--- src/components/Title1.tsx
+++ src/components/Title1.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react'
 import styles from '@/app/styles/Title.module.css';
+import Link from 'next/link';
+
 
 interface Title1Props {
   children: ReactNode
@@ -7,12 +9,12 @@ interface Title1Props {
 
 export default function Title1({ children }: Title1Props) {
   return (
-    <a href="/the-modes-website">
+    <Link href="/the-modes-website">
       <h1
         className={`md:mt-8 md:text-8xl text-5xl font-bold text-white-600 font-[family-name:var(--font-ed-celandine-outline)] mb-1 ${styles.title}`}
         >
         {children}
       </h1>
-    </a>
+    </Link>
   )
 }
