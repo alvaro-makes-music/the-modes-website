7a8ac8af942271cf66b21988f55706e40e38b674
Alvaro <alvarocaceresmunoz@gmail.com>
2025-02-23 10:48:05 -0800

    Removed extra video section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,6 @@ export default function Home() {
         <MusicSection/>
         <VideoSection/>
         <ShopSection/>
-        <VideoSection/>
         <EventsSection/>
         <BioSection/>
         <ShortGallerySection/>
