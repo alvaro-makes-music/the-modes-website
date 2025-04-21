24ddb9ac48e38b566629f7e84baa2607c61df8de
Alvaro <alvarocaceresmunoz@gmail.com>
2025-04-21 16:27:28 -0700

    Added meme image to 404 page

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -1,13 +1,23 @@
 import Link from 'next/link';
 import SectionTitle from "@/components/SectionTitle";
+import Image from "next/image";
 
 export default function Custom404() {
   return (
     <div>
       <div
         style={{ height: '200vh' }}
-        className="flex flex-col gap-y-20 mb-10 mt-10 font-[family-name:var(--font-almendra-regular)]">
+        className="flex flex-col gap-y-10 m-10 font-[family-name:var(--font-almendra-regular)]">
         <SectionTitle>404 Page not found</SectionTitle>
+        <Image
+          className="md:w-[600px]"
+          src={"media/images/404-images/" + "404.jpg"}
+          width={250}
+          height={250}
+          placeholder="blur"
+          blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAgICAgJCAkKCgkNDgwODRMREBARExwUFhQWFBwrGx8bGx8bKyYuJSMlLiZENS8vNUROQj5CTl9VVV93cXecnNEBCAgICAkICQoKCQ0ODA4NExEQEBETHBQWFBYUHCsbHxsbHxsrJi4lIyUuJkQ1Ly81RE5CPkJOX1VVX3dxd5yc0f/CABEIAAoACgMBIgACEQEDEQH/xAAoAAEBAQAAAAAAAAAAAAAAAAAFAwYBAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhADEAAAABhp4A//xAAhEAACAQMEAwEAAAAAAAAAAAABAgMAESEEBQYiEhMyQf/aAAgBAQABPwDZkR4INHEPFfX8hLhhbN8ftT8c2ETSg6xUIc3UMetcW1E67XqLTOPD2Bex6199myxySckk1//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQIBAT8Af//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQMBAT8Af//Z"
+          alt="Image showing a moon with a cat meme face"
+        />
         There may be a typo in your link, or the link may be temporarily down or obsolete.
         <br />
         <Link
